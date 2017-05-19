b554fff26d492a09269d13a6b8c5b18c5a0de648
pshah123 <priansh123@me.com>
2017-05-19 08:47:11 -0400

    dataset generation tools

---
+++ corpus_gen.py
@@ -0,0 +1,6 @@
+from pydub import AudioSegment
+import glob
+filenames = glob.glob('./*.mp3')
+for x in filenames:
+    sound = AudioSegment.from_mp3(x)
+    sound.export("./corpus/"+x[:-4]+".wav", format="wav")
